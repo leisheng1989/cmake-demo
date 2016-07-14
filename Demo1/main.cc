a1c24d75a5add945966da0d3ba3c8f5aa8956eda
TomShine <shangzongyu1111@163.com>
2016-07-14 11:34:36 +0800

    commit

--- Demo1/main.cc
+++ Demo1/main.cc
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "math/MathFunctions.h"
+
+/**
+ * power - Calculate the power of number.
+ * @param base: Base value.
+ * @param exponent: Exponent value.
+ *
+ * @return base raised to the power exponent.
+ */
+double power(double base, int exponent)
+{
+    int result = base;
+    int i;
+    
+    for(i = 1; i < exponent; ++i){
+        result = result * base;
+    }
+
+    return result;
+}
 
 int main(int argc, char *argv[])
 {
